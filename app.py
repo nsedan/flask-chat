4c7de97739553efb577a19542db1f532cd318227
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-18 17:24:42 +0000

    messages data are now in dictonary, textarea input added.

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ messages = []
 def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+
+    messages.append(messages_dict)
 
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
 
@@ -26,21 +28,25 @@ def index():
     return render_template("index.html")
 
 
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
-
-
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username, 
+        chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
     """Create a new message and rediret back to the chat page"""
     add_messages(username, message)
-    return redirect("/" + username) 
+    return redirect("/" + username)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
